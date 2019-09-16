63fe64a8edae9ec612775271a98e99a87da92641
princedubbo <201721190135@std.uestc.edu.cn>
2019-09-16 11:01:41 +0800

    init

---
+++ TreeNode.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: Prince
+ * @Date: 2019/9/16
+ * @Descripton: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
